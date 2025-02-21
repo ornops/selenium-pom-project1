21848d5a578a30aa83f67a57494a3e7e1a70c0b8
ornops <joshuaornopia@gmail.com>
2025-02-21 23:45:27 +0800

    Created automation script for login

--- Tests/login.py
+++ Tests/login.py
@@ -1,9 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-
+from selenium.webdriver.common.by import By
+import time;
 
 chrome_driver_path = "D:/Work/MG/Selenium/selenium-pom-project1/Drivers/chromedriver.exe"
 service = Service(chrome_driver_path)
 driver = webdriver.Chrome(service=service)
 
-driver = driver.get("https://cme-staging.vercel.app/login")
+driver.implicitly_wait(10)
+driver.maximize_window
+
+driver.get("https://cme-staging.vercel.app/login")
+emailInput = driver.find_element(By.XPATH,"//input[contains(@placeholder,'Email')]")
+emailInput.click()
+emailInput.send_keys('retailer_test@mail.com')
+passwordInput = driver.find_element(By.XPATH,"//input[contains(@placeholder,'Password')]")
+passwordInput.click()
+passwordInput.send_keys('PAssword123!')
+submitButton = driver.find_element(By.XPATH, "//button[contains(normalize-space(), 'Login')]")
+submitButton.click()
+time.sleep(10)
+driver.quit()
